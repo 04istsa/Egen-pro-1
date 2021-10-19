22d2aa90b18afd85a5d0bacc18176af5be78f904
Isak Tschährä <04ISTSA@skola.lidkoping.se>
2021-10-19 08:56:56 +0200

    Add project files.

---
+++ Egen pro 1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+
+namespace Egen_prodjektuppgift__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+
+            String choseNum = "0";
+
+            while (choseNum != "3")
+            {
+
+
+                Console.WriteLine("Välkommen till gissanumret");
+                Console.WriteLine("Välj ett alternativ");
+                Console.WriteLine("1. Vill du spela spelet");
+                Console.WriteLine("2. Spelets regler");
+                Console.WriteLine("3. Vill du avsluta spelet");
+                int randNum = rand.Next(1, 11);
+
+                choseNum = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (choseNum)
+                {
+
+                    case "1":
+
+                        Console.WriteLine("Skriv ett tal mellan ett och tio");
+                        int num = 0;
+                        bool valid = false;
+                        valid = int.TryParse(Console.ReadLine(), out num);
+
+                        while (!valid)
+                        {
+
+                            Console.WriteLine("Du skrev in fle alternativ, försök igen");
+                            valid = int.TryParse(Console.ReadLine(), out num);
+                        }
+
+                        while (num != randNum)
+                        {
+
+                            if (num == randNum)
+                            {
+                                Thread.Sleep(500);
+                                Console.WriteLine("Du vann");
+                                break;
+                            }
+                            else if (num < randNum)
+                            {
+                                Thread.Sleep(500);
+                                Console.WriteLine("Du gissade för för lågt");
+                                Thread.Sleep(500);
+                                Console.WriteLine("Gissa igen");
+                                valid = int.TryParse(Console.ReadLine(), out num);
+                                {
+                                    while (!valid)
+                                    {
+                                        Console.WriteLine("Du skrev fel alternativ, gissa igen");
+                                        valid = int.TryParse(Console.ReadLine(), out num);
+                                    }
+
+                                }
+
+                            }
+                            else if (num > randNum)
+                            {
+                                Thread.Sleep(500);
+                                Console.WriteLine("Du gissade för högt");
+                                Thread.Sleep(500);
+                                Console.WriteLine("Gissa igen");
+                                valid = int.TryParse(Console.ReadLine(), out num);
+                                while (!valid)
+                                {
+                                    Thread.Sleep(1000);
+                                    Console.WriteLine("Du skrev fel alternativ, gissa igen");
+                                    valid = int.TryParse(Console.ReadLine(), out num);
+                                }
+
+
+                            }
+
+
+                        }
+                        Console.WriteLine("Du vann");
+
+
+                        break;
+                    case "2":
+
+                        Console.WriteLine("Spelet går ut på att gissa på ett numer mellan ett och tio");
+                        Console.WriteLine("Om du får fel berättar datorn om ditt tal är för högt eller för lågt");
+                        Console.WriteLine("Där efter får du gissa igen tills du gissar rätt");
+                        Console.WriteLine();
+                        break;
+
+                    case "3":
+                        break;
+
+
+
+                    default:
+                        Console.WriteLine("Du skrev inte ett gilgigt alternativ");
+                        break;
+
+
+
+                }
+            }
+        }
+    }
+}
